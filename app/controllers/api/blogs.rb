18a67f1275cb9c89c8156d002702c8d6e2ca588c
Alivn <alvin.wu@taroko.io>
2018-11-13 11:00:24 +0800

    如何測試API - 打request - 從瀏覽器、Postman

--- app/controllers/api/blogs.rb
+++ app/controllers/api/blogs.rb
@@ -10,9 +10,19 @@ module API
     get '/' do
       'blogs/index'
     end
+    # 從Console測試
     # curl 'http://localhost:3000/api' 就是訪問
     # 會回'blogs/index'
     # 加-i 查看更多訊息，grape會依照副檔名回對應格式
     # curl -i 'http://localhost:3000/api.txt'
+
+    # 如果是從瀏覽器測試
+    # 要手動指定請求格式，讓Grape知道 http://localhost:3000/api.json 或api.text
+    # 如果輸入http://localhost:3000/api 會error
+    # 從Network ->Headers -> Request Headers 可以看到accept的是application/xml等
+
+    # 用Postman測試
+    # 如果Request Headers 有加KEY VALUE是 Accept text/plain
+    # 回來的Headers可以看到Content-type就會變成text/plain
   end
 end
