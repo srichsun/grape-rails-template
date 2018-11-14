272a6177bd964bc43cb823f1b74466c837ae8ddb
wu <alvin.wu@taroko.io>
2018-11-14 18:07:42 +0800

    跨域請求配置 - 如何讓跨域請求進來
    這個跟grape沒關係，rails通用的方法

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,17 @@ module GrapeRailsTemplate
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # 將 gem 'rack-cors'中間件掛載到整個rails中間件最上層
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*' #允許哪些域名可以對我們網站 跨站訪問
+        resource '*', :headers => :any, :methods => :any #[:get, :post, :options]
+        # resource表示哪些請求可以過
+        # 看network -> request 的 Headers -> Response Headers -> Access-Control-Allow-Methods
+        # 就可以看到這邊允許的method是GET, POST....，這邊跟瀏覽器講說哪些請求可以過
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
